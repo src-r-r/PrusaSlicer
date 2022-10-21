ba19ea1e4a8e030d8b9fbe6408d55507e6f1362b
Jordan Hewitt <1633901+src-r-r@users.noreply.github.com>
2022-10-21 16:20:01 -0700

    change tbb reference.

--- deps/TBB/TBB.cmake
+++ deps/TBB/TBB.cmake
@@ -4,7 +4,7 @@ prusaslicer_add_cmake_project(
     URL https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.6.0.zip
     # The original in the code
     # URL "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.5.0.zip"
-    URL_HASH 29376a78addbd0a81ac5fca5cc7ef82528281f0659b7e09a07672b136451c7b1
+    URL_HASH SHA256=29376a78addbd0a81ac5fca5cc7ef82528281f0659b7e09a07672b136451c7b1
     # The original
     # URL_HASH SHA256=83ea786c964a384dd72534f9854b419716f412f9d43c0be88d41874763e7bb47
     CMAKE_ARGS          
